4b958a366debfb94ccf1a8fe9a643fbe79725379
charlesli50 <charles.zhiyu.li@gmail.com>
2024-09-13 11:50:29 -0400

    click the thing

--- app/src/main/java/com/example/QuoteApp/MainActivity.kt
+++ app/src/main/java/com/example/QuoteApp/MainActivity.kt
@@ -8,7 +8,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.verticalArrangement
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.QuoteApp.ui.theme.QuoteAppTheme
@@ -20,9 +30,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuoteAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    val quotesArray = arrayOf(
+                        "Do or Do Not, there is not try",
+                        "Opportunities Don't Happen. You Create Them.",
+                        "If you can change your mind, you can change your life.",
+                        "Either you run your day, or your day runs you."
+                        )
+
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        quotes = quotesArray,
+                        modifier = Modifier.padding(innerPadding),
+//                        verticalArrangement = Alignment.CenterVertically
                     )
                 }
             }
@@ -31,17 +50,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(quotes: Array<String>, modifier: Modifier = Modifier) {
+
+    var num by remember { mutableStateOf(0)}
+
+    Column(modifier = modifier) {
+
+        Text(
+            text = "${quotes[num % quotes.size]}",
+//            modifier =
+        )
+
+        Button(onClick = { num += 1 }) {
+            Text("Click Me")
+        }
+    }
+
+//    Button(onClick = { num += 1 }) {
+//        Text("Click Me")
+//    }
+//
+//    Text(
+//        text = "${quotes[num%4]} \n $num",
+//        modifier = modifier
+//    )
 }
+
+val quotesArray = arrayOf(
+    "Do or Do Not, there is not try",
+    "Opportunities Don't Happen. You Create Them.",
+    "If you can change your mind, you can change your life.",
+    "Either you run your day, or your day runs you."
+)
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     QuoteAppTheme {
-        Greeting("Hello!")
+        Greeting(quotesArray)
     }
 }
