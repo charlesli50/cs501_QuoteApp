f4256177433e8b5b19a54a4842247058dcf62d45
charlesli50 <charles.zhiyu.li@gmail.com>
2024-09-13 13:49:25 -0400

    added styling, it works now

--- app/src/main/java/com/example/QuoteApp/MainActivity.kt
+++ app/src/main/java/com/example/QuoteApp/MainActivity.kt
@@ -5,21 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-//import androidx.compose.foundation.layout.verticalArrangement
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+//import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.QuoteApp.ui.theme.QuoteAppTheme
 
@@ -32,15 +33,15 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
                     val quotesArray = arrayOf(
-                        "Do or Do Not, there is not try",
-                        "Opportunities Don't Happen. You Create Them.",
-                        "If you can change your mind, you can change your life.",
-                        "Either you run your day, or your day runs you."
+                        "Do or Do Not, \nthere is not try",
+                        "Opportunities Don't Happen. \nYou Create Them.",
+                        "If you change your mind, \nyou change your life.",
+                        "Either you run your day, \nor your day runs you."
                         )
 
                     Greeting(
                         quotes = quotesArray,
-                        modifier = Modifier.padding(innerPadding),
+                        modifier = Modifier.fillMaxSize()
 //                        verticalArrangement = Alignment.CenterVertically
                     )
                 }
@@ -54,33 +55,37 @@ fun Greeting(quotes: Array<String>, modifier: Modifier = Modifier) {
 
     var num by remember { mutableStateOf(0)}
 
-    Column(modifier = modifier) {
+    Column(modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally) {
 
         Text(
             text = "${quotes[num % quotes.size]}",
-//            modifier =
+            modifier =Modifier
+                .wrapContentWidth()
+                .wrapContentHeight(align = Alignment.CenterVertically),
+//            background = Color.Black,
+//            textAlign = TextAlign.Center
+
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                color = Color.Cyan,
+                background = Color.Black,
+                fontSize = 36.sp,
+                textAlign = TextAlign.Center
+            )
         )
 
         Button(onClick = { num += 1 }) {
             Text("Click Me")
         }
     }
-
-//    Button(onClick = { num += 1 }) {
-//        Text("Click Me")
-//    }
-//
-//    Text(
-//        text = "${quotes[num%4]} \n $num",
-//        modifier = modifier
-//    )
 }
 
 val quotesArray = arrayOf(
-    "Do or Do Not, there is not try",
-    "Opportunities Don't Happen. You Create Them.",
-    "If you can change your mind, you can change your life.",
-    "Either you run your day, or your day runs you."
+    "Do or Do Not, \nthere is not try",
+//    "Opportunities Don't Happen. You Create Them.",
+    "If you change your mind, \nyou change your life.",
+    "Either you run your day, \nor your day runs you."
 )
 
 @Preview(showBackground = true)
