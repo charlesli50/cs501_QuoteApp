8c9cdfa1a05a111f83eb5261a75d63534f7ffeb3
charlesli50 <charles.zhiyu.li@gmail.com>
2024-09-13 11:22:23 -0400

    main?

--- app/src/main/java/com/example/QuoteApp/MainActivity.kt
+++ app/src/main/java/com/example/QuoteApp/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     QuoteAppTheme {
-        Greeting("Android")
+        Greeting("Hello!")
     }
 }
